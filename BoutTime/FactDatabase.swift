0992bcbcf6cc853dd725a26bc84536f80e3f716e
FBalagadde <fkb001@gmail.com>
2017-06-14 01:27:18 -0400

    incomplete but with some additional edits

--- BoutTime/FactDatabase.swift
+++ BoutTime/FactDatabase.swift
@@ -7,8 +7,12 @@
 //
 
 import Foundation
+import GameKit
 
-struct FactDatabase
+var numberOfFacterPerRound: Int = 4
+
+
+struct FactSet
 {
     let facts: [String: (factDate: String, factURL: String)] =
     [
@@ -34,3 +38,150 @@ struct FactDatabase
         "Barack Obama":             ("08-04-1961", "https://en.wikipedia.org/wiki/Barack_Obama")
     ]
 }
+
+class FactHandler
+{
+    var factSet: FactSet
+    var randomFactKey: String = ""
+    
+    init(factSet: FactSet)
+    {
+        self.factSet = factSet
+    }
+    
+    //Throw error 1 here
+    // Invalid factSet // tere is no fact with a propperly formatted date
+    // The URL error is not a show stopper so it will be handled in the app itself
+    /// input date string format: MM-DD-YYYY
+    func getStarterFacts() -> [String]
+    {
+        var indexOfSelectFact: Int = 0
+        var checkedKeys: [String] = []
+        var selectedKeys: [String] = []
+        
+        // search through the database and fetch 4 facts. 
+        // If a fact is faulty, skip it and get the next one [date does not exist]
+        // Fulty: If the date does not exist or if the date is not properly formatted
+        // Dont worry about the URL string at this point
+        
+        //Possible errors
+        //1. No valid keys found
+        //2. InvalidFactset
+        //3. Invalid URL
+        
+        repeat
+        {
+            //Obtain a random fact dictionary from a dictionary that has not yet been selected during this round
+            repeat
+            {
+                indexOfSelectFact = GKRandomSource.sharedRandom().nextInt(upperBound: factSet.facts.count)
+                
+                randomFactKey = Array(factSet.facts.keys)[indexOfSelectFact] //questionSet.triviaQuestions[indexOfSelectFact]
+                
+            } while checkedKeys.contains(randomFactKey)
+            checkedKeys.append(randomFactKey) //include the fact key in the gameQuestions array
+            
+            // If the date string exists, insert it into the return array
+            if let factDate = factSet.facts[randomFactKey]?.factDate
+            {
+                //check if the date is in teh correct format
+                if correctFormatFor(dateString: factDate)
+                {
+                    selectedKeys.append(randomFactKey)
+                    //Export only keys with valid dates
+                    //keys with invalid URLs may be exported because this is not mandatory for the game to be played
+                }
+            }
+        } while (selectedKeys.count < numberOfFacterPerRound && checkedKeys.count < factSet.facts.count)
+        
+        // FIXME: 
+        // IF you exit this loop and size of seletFactArray < 4, Throw error and exit
+ 
+        return selectedKeys
+    }
+    
+    /// Utility Function for checking is the date string is in an acceptable formatted strings
+    /// input date string format: MM-DD-YYYY
+    func correctFormatFor(dateString: String) -> Bool
+    {
+        let dateTokens = getStringTokensOf(string: dateString, delimitChar: "-")
+        
+        //var correctDateFormat: Bool = true
+        
+        if dateTokens.count != 3
+        {
+            return false
+        }
+        
+        let year = dateTokens[2]
+        
+        if let yearInt = Int(year)
+        {
+            if yearInt < 0
+            {
+                return false
+            }
+        } else {
+            return false
+        }
+        
+        
+        if year.utf8.count != 4
+        {
+            return false
+        }
+        
+        if let month = Int(dateTokens[0]), let day = Int(dateTokens[1])
+        {
+            if !(month >= 1 && month <= 12 && day >= 1 && day <= 31)
+            {
+                return false
+            }
+        } else
+        {
+            return false
+        }
+        
+        return true
+    }
+    
+    
+    
+    /// Utility Function for tokenizing delimited strings
+    func getStringTokensOf(string inputString: String, delimitChar: String) -> [String]{
+        
+        var tokens: [String] = []
+        var tempString = ""
+        
+        for letter in inputString.characters {
+            
+            switch String(letter) {
+            case String(delimitChar):
+                tokens.append(tempString)
+                tempString = ""
+            default:
+                tempString += String(letter)
+            }
+        }
+        tokens.append(tempString)
+        return tokens
+    } //end func getStringTokensOf
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- BoutTime/ViewController.swift
+++ BoutTime/ViewController.swift
@@ -27,18 +27,127 @@ class ViewController: UIViewController {
     @IBOutlet weak var button3DownSelected: UIButton!
     @IBOutlet weak var button4UpSelected: UIButton!
     
+    
+    @IBOutlet weak var button1Down: UIButton!
+    @IBOutlet weak var button2Up: UIButton!
+    @IBOutlet weak var button2Down: UIButton!
+    @IBOutlet weak var button3Up: UIButton!
+    @IBOutlet weak var button3Down: UIButton!
+    @IBOutlet weak var button4Up: UIButton!
+    
+    
+    @IBOutlet weak var fact1Button: UIButton!
+    @IBOutlet weak var fact2Button: UIButton!
+    @IBOutlet weak var fact3Button: UIButton!
+    @IBOutlet weak var fact4Button: UIButton!
+    
     @IBOutlet weak var nextRoundSuccess: UIButton!
     @IBOutlet weak var nextRoundFail: UIButton!
     
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var messageLabel: UILabel!
     
+    let famousBirthdays: FactSet = FactSet()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         initializeAppObjects()
+        canvas()
+        startGame()
     }
+    
+    func startGame()
+    {
+        let factHandler = FactHandler(factSet: famousBirthdays)
+        
+        let factList = factHandler.getStarterFacts()
+        for fact in factList
+        {
+            print(fact)
+        }
+        
+        populateLabelsWithFacts(from: factList)
+    }
+    
+    func populateLabelsWithFacts(from factList: [String])
+    {
+        insertFactInto(label: factLabel1, fact: factList[0])
+        insertFactInto(label: factLabel2, fact: factList[1])
+        insertFactInto(label: factLabel3, fact: factList[2])
+        insertFactInto(label: factLabel4, fact: factList[3])
+    }
+    
+    func canvas()
+    {
+        
+    }
+    
+    func exchangeLabelFactsFor(label1: UILabel, label2: UILabel)
+    {
+        let fact1 = label1.text
+        label1.text = label2.text
+        label2.text = fact1
+    }
+    
+    func insertFactInto(label: UILabel, fact: String)
+    {
+        label.text = fact
+    }
+    
+    
+    
+    
+    @IBAction func moveFact(_ sender: UIButton)
+    {
+        switch sender
+        {
+        case button1Down, button2Up:
+            enableAllButtons(false)
+            exchangeLabelFactsFor(label1: factLabel1, label2: factLabel2)
+            enableAllButtons(true)
+            
+        case button2Down, button3Up:
+            enableAllButtons(false)
+            exchangeLabelFactsFor(label1: factLabel2, label2: factLabel3)
+            enableAllButtons(true)
+            
+        case button3Down, button4Up:
+            enableAllButtons(false)
+            exchangeLabelFactsFor(label1: factLabel3, label2: factLabel4)
+            enableAllButtons(true)
+            
+            //Need to wire these towards the end with URL connectivity
+        case fact1Button: print("test")
+        case fact2Button: print("test")
+        case fact3Button: print("test")
+        case fact4Button: print("test")
+            
+        default: print("The default statement in func moveFact() has been executed. This should not be happening. Fix error!")
+        }
+    }
+    
+    func enableAllButtons(_ state: Bool)
+    {
+        button1Down.isUserInteractionEnabled   = state
+        button2Up.isUserInteractionEnabled     = state
+        button2Down.isUserInteractionEnabled   = state
+        button3Up.isUserInteractionEnabled     = state
+        button3Down.isUserInteractionEnabled   = state
+        button4Up.isUserInteractionEnabled     = state
+    }
+    
+    //@IBAction func moveFact(_ sender: UIButton)
+    //{
+    //    switch sender
+    //    {
+    //    case
+    //    default:
+    //        print("nothing")
+    //    }
+    //}
+   
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -58,24 +167,38 @@ class ViewController: UIViewController {
         factLabel3.text = ""
         factLabel4.text = ""
         
-        button1DownSelected.isHidden = true
-        button2UpSelected.isHidden = true
-        button2DownSelected.isHidden = true
-        button3UpSelected.isHidden = true
-        button3DownSelected.isHidden = true
-        button4UpSelected.isHidden = true
+        //button1DownSelected.isHidden = true
+        //button2UpSelected.isHidden = true
+        //button2DownSelected.isHidden = true
+        //button3UpSelected.isHidden = true
+        //button3DownSelected.isHidden = true
+        //button4UpSelected.isHidden = true
+        
+        button1Down.isHidden = false
+        button2Up.isHidden = false
+        button2Down.isHidden = false
+        button3Up.isHidden = false
+        button3Down.isHidden = false
+        button4Up.isHidden = false
+        
+        button1Down.isEnabled = true
+        button2Up.isEnabled = true
+        button2Down.isEnabled = true
+        button3Up.isEnabled = true
+        button3Down.isEnabled = true
+        button4Up.isEnabled = true
         
         factFrame1.layer.cornerRadius = cornerRadius
         factFrame2.layer.cornerRadius = cornerRadius
         factFrame3.layer.cornerRadius = cornerRadius
         factFrame4.layer.cornerRadius = cornerRadius
         
-        button1DownSelected.isEnabled = false
-        button2UpSelected.isEnabled = false
-        button2DownSelected.isEnabled = false
-        button3UpSelected.isEnabled = false
-        button3DownSelected.isEnabled = false
-        button4UpSelected.isEnabled = false
+        button1DownSelected.isUserInteractionEnabled = false
+        button2UpSelected.isUserInteractionEnabled = false
+        button2DownSelected.isUserInteractionEnabled = false
+        button3UpSelected.isUserInteractionEnabled = false
+        button3DownSelected.isUserInteractionEnabled = false
+        button4UpSelected.isUserInteractionEnabled = false
         
         nextRoundSuccess.isHidden = true
         nextRoundFail.isHidden = true
